da74fb22c65b117bf890b7e9c5762f6eda10db4a
unknown v2 <unknownv2@users.noreply.github.com>
2018-07-04 21:02:50 -0400

    Remove unused cli.c code

--- Linux/cli.c
+++ Linux/cli.c
@@ -1,21 +1,3 @@
 #include "cli.h"
 #include "elf-parse.h"
 
-#define _print_line_sep (printf("--------------------------------------------------------------\n"))
-
-void print_welcome()
-{
-    printf(GRN);
-    _print_line_sep;
-    printf("%s - (%s) - by %s\n", PROGRAM_NAME, PROGRAM_VER, PROGRAM_AUTHOR);
-    _print_line_sep;
-    printf(RESET);
-}
-
-
-void print_usage()
-{
-  printf(GRN);
-  printf("usage: sudo ./inject <pid> </path/evil.so>");
-  printf(RESET);
-}
