895edf5107875e6c3fda555f94750cd1e75025d6
unknown v2 <unknownv2@users.noreply.github.com>
2018-07-04 21:01:50 -0400

    Remove unused code and add arm64 build options

--- Linux/elf-parse.h
+++ Linux/elf-parse.h
@@ -32,7 +32,11 @@
 #define link_map_public link_map
 #include <link.h>
 #undef link_map
-
+#if defined(__GNUC__)
+#define INJECT_EXPORT __attribute__((visibility("default")))
+#else
+#define INJECT_EXPORT
+#endif
 // linker's link_map
 typedef struct dyn_info
 {
--- Linux/utils.c
+++ Linux/utils.c
@@ -1,20 +1,6 @@
 #include "utils.h"
 
-unsigned char
-    soloader[] =
-	"\x90"
-	"\xeb\x13"
-	"\x58"
-	"\xba\x01\x00\x00\x00"
-	"\x52"
-	"\x50"
-	"\xbb\x03\x00\x00\x00"
-	"\xff\xd3"
-	"\x83\xc4\x08"
-	"\xcc"
-	"\xe8\xe8\xff\xff\xff";
-
-void ptrace_attach(int pid)
+INJECT_EXPORT void ptrace_attach(int pid)
 {
     if ((ptrace(PTRACE_ATTACH, pid, NULL, NULL)) < 0)
     {
@@ -25,7 +11,7 @@ void ptrace_attach(int pid)
     waitpid(pid, NULL, WUNTRACED);
 }
 
-void ptrace_cont(int pid)
+INJECT_EXPORT void ptrace_cont(int pid)
 {
     int s;
 
@@ -39,7 +25,7 @@ void ptrace_cont(int pid)
 	waitpid(pid, &s, WNOHANG);
 }
 
-void ptrace_detach(int pid)
+INJECT_EXPORT void ptrace_detach(int pid)
 {
     if (ptrace(PTRACE_DETACH, pid, NULL, NULL) < 0)
     {
@@ -48,7 +34,7 @@ void ptrace_detach(int pid)
     }
 }
 
-bool ptrace_read(int pid, unsigned long addr, void *data, unsigned int len)
+INJECT_EXPORT bool ptrace_read(int pid, unsigned long addr, void *data, unsigned int len)
 {
     int bytesRead = 0;
     int i = 0;
@@ -105,7 +91,7 @@ ptrace_read_string(int pid, unsigned long start)
     return NULL;
 }
 
-void ptrace_write(int pid, unsigned long addr, void *vptr, int len)
+INJECT_EXPORT void ptrace_write(int pid, unsigned long addr, void *vptr, int len)
 {
     int byteCount = 0;
     long word = 0;
@@ -129,44 +115,4 @@ void setaddr(unsigned char *buf, ElfW(Addr) addr)
     *(buf + 1) = addr >> 8;
     *(buf + 2) = addr >> 16;
     *(buf + 3) = addr >> 24;
-}
-
-void
-inject_code(int pid, char *evilso, long dlopen_addr, long inject_position) {
-	struct	user_regs_struct regz, regzbak;
-	unsigned long len;
-	unsigned char *backup = NULL;
-	unsigned char *loader = NULL;
-
-	setaddr(soloader + 12, dlopen_addr);
-
-	printf("[+] entry point: 0x%x\n", inject_position);
-
-	len = sizeof(soloader) + strlen(evilso);
-	loader = malloc(sizeof(char)  *len);
-	memcpy(loader, soloader, sizeof(soloader));
-	memcpy(loader+sizeof(soloader) - 1 , evilso, strlen(evilso));
-
-	backup = malloc(len + sizeof(long));
-	ptrace_read(pid, inject_position, backup, len);
-
-	if(ptrace(PTRACE_GETREGS , pid , NULL , &regz) < 0) exit(-1);
-	if(ptrace(PTRACE_GETREGS , pid , NULL , &regzbak) < 0) exit(-1);
-	printf("[+] stopped %d at eip:%p, esp:%p\n", pid, regz.eip, regz.esp);
-
-	regz.eip = inject_position + 2;
-
-	/*code inject */
-	ptrace_write(pid, inject_position, loader, len);
-
-	/*set eip as entry_point */
-	ptrace(PTRACE_SETREGS , pid , NULL , &regz);
-	ptrace_cont(pid);
-
-	if(ptrace(PTRACE_GETREGS , pid , NULL , &regz) < 0) exit(-1);
-	printf("[+] inject code done %d at eip:%p\n", pid, regz.eip);
-
-	/*restore backup data */
-	// ptrace_write(pid,entry_addr, backup, len);
-	ptrace(PTRACE_SETREGS , pid , NULL , &regzbak);
 }
